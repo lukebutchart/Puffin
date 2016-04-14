566250b567b4bc7ffeb4d4daf498f419ac552907
Luke Butchart <Luke.Butchart@allocatesoftware.com>
2016-04-14 16:37:06 +0100

    LOTS of work with Xamarin

--- Puffin/MainActivity.cs
+++ Puffin/MainActivity.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 
@@ -11,21 +8,60 @@ namespace Puffin
     [Activity(Label = "Puffin", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            DatabaseUpdates mydata = AppManager.DBUpdates;
+
+            mydata.SetContext(this);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            Button playButton = FindViewById<Button>(Resource.Id.PlayButton);
+            Button collectionButton = FindViewById<Button>(Resource.Id.CollectionButton);
+            Button shopButton = FindViewById<Button>(Resource.Id.ShopButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            foreach (Collectable coll in AppManager.Collectables)
+            {
+                mydata.AddCollectable(coll);
+            }
+
+
+            playButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Puffin.PlayActivity));
+                //intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
+
+
+            collectionButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Puffin.CollectionActivity));
+                StartActivity(intent);
+            };
+
+            shopButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Puffin.ShopActivity));
+                StartActivity(intent);
+            };
         }
+
+
+
+
+
+        //protected override void OnResume(Bundle bundle)
+        //{
+        //}
+
+        //protected override void OnPause(Bundle bundle)
+        //{
+        //}
     }
 }
-
