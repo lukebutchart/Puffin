dfbb3bfeb74fa7faa0f847a546af594fda0657bb
Luke Butchart <Luke.Butchart@allocatesoftware.com>
2016-04-18 14:58:38 +0100

    Added Friend class and CollectionActivity information.

--- Puffin/MainActivity.cs
+++ Puffin/MainActivity.cs
@@ -3,10 +3,11 @@ using Android.Content;
 using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
+using System;
 
 namespace Puffin
 {
-    [Activity(Label = "Puffin", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Puffin", MainLauncher = true, Icon = "@drawable/Puffin")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -25,8 +26,10 @@ namespace Puffin
             Button playButton = FindViewById<Button>(Resource.Id.PlayButton);
             Button collectionButton = FindViewById<Button>(Resource.Id.CollectionButton);
             Button shopButton = FindViewById<Button>(Resource.Id.ShopButton);
+            Button resetButton = FindViewById<Button>(Resource.Id.ResetButton);
 
             List<string> allCollNames = new List<string>();
+            List<string> allFriendNames = new List<string>();
 
             foreach (Collectable coll in mydata.GetAllCollectables())
             {
@@ -38,6 +41,19 @@ namespace Puffin
                 if (!allCollNames.Contains(coll.Name))
                 {
                     mydata.AddCollectable(coll);
+                }
+            }
+
+            foreach (Friend friend in mydata.GetAllFriends())
+            {
+                allFriendNames.Add(friend.Name);
+            }
+
+            foreach (Friend friend in AppManager.Friends)
+            {
+                if (!allFriendNames.Contains(friend.Name))
+                {
+                    mydata.AddFriend(friend);
                 }
             }
 
@@ -62,11 +78,46 @@ namespace Puffin
                 var intent = new Intent(this, typeof(Puffin.ShopActivity));
                 StartActivity(intent);
             };
+
+            resetButton.Click += (sender, e) =>
+            {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+                alert.SetTitle(String.Format("Are you sure you want to reset all data?"));
+
+                alert.SetPositiveButton("Yes", (senderAlert, args) => {
+                    foreach (Collectable coll in AppManager.DBUpdates.GetAllCollectables())
+                    {
+                        AppManager.DBUpdates.DeleteCollectable(coll);
+                    }
+                    foreach (Player player in AppManager.DBUpdates.GetAllPlayers())
+                    {
+                        AppManager.DBUpdates.DeletePlayer(player);
+                    }
+                    foreach (Friend friend in AppManager.DBUpdates.GetAllFriends())
+                    {
+                        AppManager.DBUpdates.DeleteFriend(friend);
+                    }
+
+                    foreach (Collectable coll in AppManager.Collectables)
+                    {
+                        mydata.AddCollectable(coll);
+                    }
+                    mydata.AddPlayer(new Player(AppManager.PlayerName));
+                    foreach (Friend friend in AppManager.Friends)
+                    {
+                        mydata.AddFriend(friend);
+                    }
+                });
+
+                alert.SetNegativeButton("No", (senderAlert, args) => {                    
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+            };
         }
-
-
-
-
+        
 
         //protected override void OnResume(Bundle bundle)
         //{
